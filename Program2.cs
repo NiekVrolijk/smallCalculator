b809d22956d0f56075b0a8484eae67bed1e75d63
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-09-18 16:24:29 +0200

    added 2 new thingies

---
+++ Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smallCalculator
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, guys!");
+            //test
+            //test2
+        }
+    }
+}
