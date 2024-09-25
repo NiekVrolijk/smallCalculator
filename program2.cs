36b9242fc89e65903c6dbe68d02bf2ba7f171ba4
LoekLucas <l.van.kessel@sintlucasedu.nl>
2024-09-25 16:13:23 +0200

    Added funny check to add/subtract

--- program2.cs
+++ program2.cs
@@ -4,35 +4,68 @@ namespace smallCalculator
 {
     internal class Program2
     {
-
-        float userInput;
-        float addAmount;
-        float subtractAmount;
-
         public void Add()
         {
             Console.Clear();
             Console.WriteLine("What number would you like to start with?");
-            userInput = Console.ReadLine();
+
+            double userInput;
+            if (!double.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Add();
+                return;
+            }
+
             Console.Clear();
             Console.WriteLine("Now how much would you like to add to " + userInput + "?");
-            addAmount = Console.ReadLine();
+
+            double addAmount;
+            if (!double.TryParse(Console.ReadLine(), out addAmount))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Add();
+                return;
+            }
+
             Console.Clear();
-            Console.WriteLine("Here's your answer!")
-            Console.WriteLine(userInput += multiplyAmount);
+            Console.WriteLine("Here's your answer!");
+            Console.WriteLine(userInput + addAmount);
         }
 
         public void Subtract()
         {
             Console.Clear();
             Console.WriteLine("What number would you like to start with?");
-            userInput = Console.ReadLine();
+
+            double userInput;
+            if (!double.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Subtract();
+                return;
+            }
+
             Console.Clear();
             Console.WriteLine("Now how much would you like to subtract from " + userInput + "?");
-            subtractAmount = Console.ReadLine();
+
+            double subtractAmount;
+            if (!double.TryParse(Console.ReadLine(), out subtractAmount))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Subtract();
+                return;
+            }
+
             Console.Clear();
-            Console.WriteLine("Here's your answer!")
-            Console.WriteLine(userInput -= multiplyAmount);
+            Console.WriteLine("Here's your answer!");
+            Console.WriteLine(userInput - subtractAmount);
         }
+
     }
+}
 }
