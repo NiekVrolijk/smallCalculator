cfa2230368133b96c1da00829a1df6e5d7c310af
LoekLucas <l.van.kessel@sintlucasedu.nl>
2024-09-25 16:08:04 +0200

    Added funny check to multiply and divide

--- program3.cs
+++ program3.cs
@@ -16,28 +16,65 @@ namespace smallCalculator
 
         public void Multiply()
         {
+            double userInput;
+            double multiplyAmount;
+
             Console.Clear();
             Console.WriteLine("What number would you like to multiply?");
-            userInput = Console.ReadLine();
+
+            if (!double.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Multiply();
+                return;
+            }
+
             Console.Clear();
             Console.WriteLine("Now how much would you like to multiply that number?");
-            multiplyAmount = Console.ReadLine();
+
+            if (!double.TryParse(Console.ReadLine(), out multiplyAmount))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Multiply();
+                return;
+            }
+
             Console.Clear();
-            Console.WriteLine("Here's your answer!")
-            Console.WriteLine(userInput *= multiplyAmount);
+            Console.WriteLine("Here's your answer!");
+            Console.WriteLine(userInput * multiplyAmount);
         }
 
         public void Divide()
         {
             Console.Clear();
             Console.WriteLine("What number would you like to divide?");
-            userInput = Console.ReadLine();
+
+            double userInput;
+            if (!double.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Divide();
+                return;
+            }
+
             Console.Clear();
             Console.WriteLine("Now how much would you like to divide that number?");
-            divideAmount = Console.ReadLine();
+
+            double divideAmount;
+            if (!double.TryParse(Console.ReadLine(), out divideAmount))
+            {
+                Console.WriteLine("\nERROR; try again");
+                Thread.Sleep(1000);
+                Divide();
+                return;
+            }
+
             Console.Clear();
-            Console.WriteLine("Here's your answer!")
-            Console.WriteLine(userInput *= divideAmount);
+            Console.WriteLine("Here's your answer!");
+            Console.WriteLine(userInput / divideAmount);
         }
     }
 }
