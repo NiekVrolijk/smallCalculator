22ba08a68ae85cbb52f96a9953ddde11310e784b
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-09-25 14:26:13 +0200

    added welkom message and a comment for wrong inputs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,65 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace smallCalculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello, guys!");
-            //test
-            //test2
+            int playerOption;
 
-            LoekFunction();
+            
+            Console.WriteLine("Welkom to our small calculater!");
+            AskPlayerOption();
 
-            Console.WriteLine("SECOND HELLO");
+            void AskPlayerOption()
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. adding");
+                Console.WriteLine("2. subtrackting");
+                Console.WriteLine("3. multiply");
+                Console.WriteLine("4. divide");
+                playerOption = ReadIntInput();
+                switch (playerOption)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                }
+            }
+
+            int ReadIntInput()
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (int.TryParse(input, out int result))
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR; try again");
+                    }
+                }
+            }
+
+            //else
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("ERROR; try again");
+            //    [void here]();
+            //}
         }
 
-        static void LoekFunction()
-        {
-            Console.WriteLine("Loek's function");
-        }
     }
 }
